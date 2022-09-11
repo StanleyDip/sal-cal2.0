6f112048bf7ee2299f69f3d53f943c8733855632
StanleyDip <stanleydr77@gmail.com>
2022-09-12 05:50:23 +0600

    Error msg updated

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 const arr=[];
-document.getElementById("submit").onclick =function(){
+const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+const d = new Date();
+let month = months[d.getMonth()];
+const mon = document.getElementById('mon');
+mon.innerHTML='Calculate The Salary of Month: '+month+"&nbsp;&nbsp;&nbsp;<i id='re' onClick='window.location.reload();' class='fa-solid fa-rotate-right'></i>";
+const form =document.getElementById('form');
+
+form.addEventListener('submit', (e)=>{
+e.preventDefault();
 arr["SalaryAmount"]=parseInt(document.getElementById('sal-amount').value);
 arr["NightShifts"]=parseInt(document.getElementById('night-shifts').value);
 arr["OTs"]=parseInt(document.getElementById('ots').value);
@@ -7,20 +15,21 @@ arr["EidBonus"]=document.getElementById('eid-bonus').value;
 arr["EidCoverage"]=parseInt(document.getElementById('eid-cov').value);
 arr["Food"]=parseInt(document.getElementById('food').value);
     calculate();
-    // console.log(arr);
+});
+
+
  
-}
+
 
 function calculate(){
+    let success= true;
     const pattern= /^[0-9]\d*$/;
-   if(!(pattern.test(arr.SalaryAmount) && pattern.test(arr.NightShifts)&& pattern.test(arr.OTs) && pattern.test(arr.Food))){
-    console.log("hit");
-//  console.log("sal-test "+pattern.test(arr.SalaryAmount)+" type of " + typeof arr.SalaryAmount+ arr.SalaryAmount);
-//  console.log("Nightshift-test "+pattern.test(arr.NightShifts)+" type of " + typeof arr.NightShifts+ arr.NightShifts);
-//  console.log("Ots-test "+pattern.test(arr.OTs)+" type of " + typeof arr.OTs+ arr.OTs);
-//  console.log("Food-test "+pattern.test(arr.Food)+" type of " + typeof arr.Food+ arr.Food);
+    const pattern1= /^[1-9]\d*$/;
+   if(!(pattern1.test(arr.SalaryAmount) && pattern.test(arr.NightShifts)&& pattern.test(arr.OTs) && pattern.test(arr.Food))){
+    
+//  window.location.reload();
+success= false;
  
- window.location.reload();
    }
         
        
@@ -35,8 +44,17 @@ function calculate(){
         calculation= calculation+400*arr.NightShifts+(arr.SalaryAmount/30)*1.5*arr.OTs+(arr.SalaryAmount/30)*2.85*arr.EidCoverage;
         calculation= calculation-arr.Food*30;
         if(!(isNaN(calculation))){
-            document.getElementById('submit').remove();
-            document.getElementById('result-div').innerHTML="You have got approximately: "+ calculation +"Tk"+ "";
+            if(success==true){
+                document.getElementById('submit').remove();
+                document.getElementById('error').remove();
+                mon.style.opacity="1";
+                mon.innerHTML=month+" Month's Salary: &nbsp;&nbsp;&nbsp;<i id='re' onClick='window.location.reload();' class='fa-solid fa-rotate-right'></i>";
+                document.getElementById('result-div').innerHTML="You have got approximately: "+ calculation +"Tk"+ "";
+            }
+            else{
+                mon.style.opacity="0";
+                document.getElementById('error').innerHTML="Please try again!! &nbsp;&nbsp;&nbsp;<i id='re-re' onClick='window.location.reload();' class='fa-solid fa-rotate-right'></i>";
+            }
         }
  
 
